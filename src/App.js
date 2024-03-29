ef7f38e6ca74233a0494c825ac878017523bba81
Zeki Karakaya <zeki1907801@gmail.com>
2024-03-29 11:28:13 +0200

    eklendi

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
 <Input type="text" value={username} label="Phone Number,username or email" onChange={(e)=>setUsername(e.target.value)}></Input>
     <Input type="password" value={password} label="Password" onChange={(e)=>setPassword(e.target.value)}></Input>
+    <p>Elero</p>
     </div>
   );
 }
