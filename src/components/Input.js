828aebd312f21fd7ee6a94727a9e4faf94ea36db
Zeki Karakaya <zeki1907801@gmail.com>
2024-04-29 16:13:18 +0300

    add

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import { useState,useReducer,useEffect,useRef } from "react";
+import {useEffect, useRef, useState} from "react";
 
-const Input = ({label,type,value,onChange}) => {
-    const inputRef =useRef()
-    const [show,setShow]=useState(false);
-    const [inputType,setType]=useState("text");
-    useEffect(() => {
-      if(inputType=="text"){
-        setType("password");
-         inputRef.current.focus()
-      }else{
-        setType("text");
-      }
-    }, [show])
-    
-  return (
-    <label className="block relative w-[250px] mb-2">
-			<input required={true} ref={inputRef} type={inputType} className="bg-zinc-50 border px-2 w-full h-[38px] rounded-sm outline-none focus:border-gray-400 peer text-sm valid:pt-[10px]" onChange={onChange} ></input>
-			<small className="absolute top-1/2 left-2 -translate-y-1/2 text-xs text-gray-500 cursor-text pointer-events-none peer-valid:text-[10px] transition-all peer-valid:top-2">{label}</small>
-            {
-                type === "password" && value && (
-                    <button type='button' onClick={()=>setShow(!show)} className='absolute right-0 top-0 h-full flex items-center'>
-                        {
-                            show ? "Hide": "Show"
-                        }
-                    </button>
-                    
-                )
-            }
-		   </label>
-  )
+export default function Input({label, type = 'text', ...props}) {
+
+	const inputRef = useRef()
+	const [show, setShow] = useState(false)
+	const [inputType, setType] = useState(type)
+
+	useEffect(() => {
+		if (show) {
+			setType('text')
+			inputRef.current.focus()
+		} else if (type === 'password') {
+			setType('password')
+			inputRef.current.focus()
+		}
+	}, [show])
+
+	return (
+		<label className="block relative flex bg-zinc-50 border rounded-sm focus-within:border-gray-400">
+			<input ref={inputRef} required={true} type={inputType} className="px-2 outline-none text-xs w-full h-[38px] valid:pt-[10px] peer" {...props}/>
+			<small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5">{label}</small>
+			{type === 'password' && props?.value && (
+				<button type="button" onClick={() => setShow(show => !show)} className="h-full flex items-center text-sm font-semibold pr-2">
+					{show ? 'Hide' : 'Show'}
+				</button>
+			)}
+		</label>
+	)
 }
-
-export default Input
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    container:{
-      center: true,
+    extend: {
+      colors: {
+        brand: '#0095f6',
+        facebook: '#385185',
+        link: '#00376b',
+        cv:"#303846",
+      },
+      backgroundImage: {
+        'logo-pattern': 'url(https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png)',
+      },
+      fontFamily:{
+            body: ["Rubik"]
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
-
